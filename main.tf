2eadcfc1d2ece69ca4915d14d8ed5aeda4eb0c4e
zit-git-jzan <johann@zandanel.net>
2025-02-25 13:16:30 +0100

    2

--- main.tf
+++ main.tf
@@ -4,5 +4,12 @@ data "alicloud_zones" "default" {
 
 resource "alicloud_vpc" "vpc" {
   vpc_name   = "my-terraform-vpc"
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "alicloud_vswitch" "foo" {
+ vswitch_name = "test1"
+ cidr_block = "10.0.0.0/16"
+ vpc_id = alicloud_vpc.vpc.id
+ zone_id = data.alicloud_zones.default.id
 }
