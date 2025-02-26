a17a1fde35d7d1061a172ef24d7965addd14675f
zit-git-jzan <johann@zandanel.net>
2025-02-26 21:36:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ resource "alicloud_vpc" "vpc" {
 }
 
 resource "alicloud_vswitch" "foo" {
- vswitch_name = "test1"
- cidr_block = "10.0.0.0/16"
- vpc_id = alicloud_vpc.vpc.id
- zone_id = data.alicloud_zones.default.id
+  vswitch_name = "test1"
+  cidr_block   = "10.0.0.0/16"
+  vpc_id       = alicloud_vpc.vpc.id
+  zone_id      = data.alicloud_zones.default.id
 }
